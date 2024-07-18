09a94e4954bc4c20b4bc76c0ad8cdc5b2a2365b8
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2024-07-18 13:44:39 +0200

    Removing old registry code
    Signed-off-by: Janos Bonic <86970079+janosdebugs@users.noreply.github.com>

--- internal/util.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-import "strings"
-
-// TODO: Move this somewhere to it's own package, not in internal.
-
-func TrimTagPrefix(version string) string {
-	return strings.TrimPrefix(version, "v")
-}
