f3951ea333c7e3e4db15ff59866329b20758e74b
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2024-07-19 10:13:49 +0200

    Added backfill test
    Signed-off-by: Janos Bonic <86970079+janosdebugs@users.noreply.github.com>

--- module_update_test.go
+++ module_update_test.go
@@ -2,9 +2,7 @@ package libregistry_test
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"os"
+	"strconv"
 	"testing"
 
 	"github.com/opentofu/libregistry"
@@ -13,52 +11,14 @@ import (
 	"github.com/opentofu/libregistry/types/module"
 	"github.com/opentofu/libregistry/vcs"
 	"github.com/opentofu/libregistry/vcs/fakevcs"
-	"github.com/opentofu/libregistry/vcs/github"
 )
 
-func ExampleAPI_AddModule() {
-	ghClient, err := github.New(os.Getenv("GITHUB_TOKEN"), nil)
-	if err != nil {
-		panic(err)
-	}
+// TestUpdateModuleBackfill tests that the process correctly backfills using the expensive ListAllVersions call when
+// it detects that there was no overlap between the existing versions and the versions returned by ListLatestVersions.
+// This test relies on the fact that the fakevcs returns 5 versions in the ListLatestVersions call.
+func TestUpdateModuleBackfill(t *testing.T) {
+	const createVersionCount = 20
 
-	storage := memory.New()
-
-	dataAPI, err := metadata.New(storage)
-	if err != nil {
-		panic(err)
-	}
-
-	registry, err := libregistry.New(
-		ghClient,
-		dataAPI,
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	ctx := context.Background()
-
-	if err := registry.AddModule(ctx, "terraform-aws-modules/terraform-aws-iam"); err != nil {
-		panic(err)
-	}
-
-	// Manually read the registry data file:
-	jsonFile, err := storage.GetFile(ctx, "modules/t/terraform-aws-modules/iam/aws.json")
-	if err != nil {
-		panic(err)
-	}
-
-	var data map[string]any
-	if err := json.Unmarshal(jsonFile, &data); err != nil {
-		panic(err)
-	}
-	fmt.Printf("Latest version: %s", data["versions"].([]any)[0].(map[string]any)["version"].(string))
-}
-
-// TestAddModule tests that a module, when added from a repository, correctly appears in the metadata storage with
-// the correct version information.
-func TestAddModule(t *testing.T) {
 	inMemoryVCS := fakevcs.New()
 	storage := memory.New()
 	ctx := context.Background()
@@ -105,16 +65,30 @@ func TestAddModule(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	for i := 1; i <= createVersionCount; i++ {
+		if err := inMemoryVCS.CreateVersion(repo, "v1.0."+strconv.Itoa(i)); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	if err := registry.UpdateModule(ctx, moduleAddr); err != nil {
+		t.Fatal(err)
+	}
+
 	storedMetadata, err := dataAPI.GetModule(ctx, moduleAddr)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(storedMetadata.Versions) != 1 {
+	if len(storedMetadata.Versions) != createVersionCount+1 {
 		t.Fatalf("Incorrect number of versions: %d", len(storedMetadata.Versions))
 	}
 
-	if ver := storedMetadata.Versions[0].Version; ver != "v1.0.0" {
-		t.Fatalf("Incorrect version stored: %s", ver)
+	j := 0
+	for i := createVersionCount; i >= 0; i-- {
+		if ver := storedMetadata.Versions[j].Version; ver != module.VersionNumber("v1.0."+strconv.Itoa(i)) {
+			t.Fatalf("Incorrect version in position %d: %s", j, ver)
+		}
+		j++
 	}
 }
