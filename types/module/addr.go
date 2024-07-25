d674877040d2b2eedfdda10db0ef26d5e6e97ae9
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2024-07-25 18:36:54 +0200

    JSON marshalling for module.Addr
    Signed-off-by: Janos Bonic <86970079+janosdebugs@users.noreply.github.com>

--- types/module/addr.go
+++ types/module/addr.go
@@ -4,7 +4,9 @@
 package module
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/opentofu/libregistry/vcs"
 	regaddr "github.com/opentofu/registry-address"
@@ -29,6 +31,28 @@ func (a Addr) Validate() error {
 	return nil
 }
 
+func (a Addr) MarshalJSON() ([]byte, error) {
+	// Note: this intentionally doesn't have a pointer receiver! Don't add one!
+	normalized := a.Normalize()
+	//goland:noinspection GoRedundantConversion
+	return json.Marshal(string(normalized.Namespace + "/" + normalized.Name + "/" + normalized.TargetSystem))
+}
+
+func (a *Addr) UnmarshalJSON(b []byte) error {
+	var data string
+	if err := json.Unmarshal(b, &data); err != nil {
+		return err
+	}
+	parts := strings.Split(data, "/")
+	if len(parts) != 3 {
+		return fmt.Errorf("invalid module address: %s", data)
+	}
+	a.Namespace = parts[0]
+	a.Name = parts[1]
+	a.TargetSystem = parts[2]
+	return nil
+}
+
 func (a Addr) Normalize() Addr {
 	return NormalizeAddr(a)
 }
@@ -41,7 +65,7 @@ func (a Addr) String() string {
 func (a Addr) Equals(other Addr) bool {
 	current := a.Normalize()
 	other = other.Normalize()
-	return current.Namespace == other.Namespace && current.Name == other.Namespace && current.TargetSystem == other.TargetSystem
+	return current.Namespace == other.Namespace && current.Name == other.Name && current.TargetSystem == other.TargetSystem
 }
 
 func (a Addr) ToRepositoryAddr() vcs.RepositoryAddr {
---
+++ types/module/addr_test.go
@@ -0,0 +1,31 @@
+package module_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/opentofu/libregistry/types/module"
+)
+
+func TestAddrJSON(t *testing.T) {
+	const testNamespace = "opentofu"
+	const testName = "test"
+	const testSystem = "aws"
+
+	moduleAddr := module.Addr{
+		Namespace:    testNamespace,
+		Name:         testName,
+		TargetSystem: testSystem,
+	}
+	marshalled, err := json.Marshal(moduleAddr)
+	if err != nil {
+		t.Fatalf("Failed to marshal module address (%v)", err)
+	}
+	moduleAddr2 := module.Addr{}
+	if err := json.Unmarshal(marshalled, &moduleAddr2); err != nil {
+		t.Fatalf("Failed to unmarshal module address (%v)", err)
+	}
+	if !moduleAddr.Equals(moduleAddr2) {
+		t.Fatalf("Module addresses are not equal.")
+	}
+}
